c3d04b2f028b2d10a03ecee686fa8cc9a5616163
micky rendon <elderendon@hotmail.es>
2023-09-07 12:17:48 -0300

    [test] testeando el preloader

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const body = document.querySelector('body')
 
+
+
 const menuEvent = (e) => {
     const menuM = document.querySelector('.menu-mobile')
 
@@ -22,12 +24,12 @@ body.addEventListener('click', menuEvent, false)
 const serviceImpar = () => {
     const servicesCard =  document.querySelectorAll('.service-card')
     const newArray = [...servicesCard]
-    console.log(newArray)
+    // console.log(newArray)
 
     newArray.forEach((element, idx) => {
         const impar = idx % 2 !== 0
         const trues = impar[idx] !== true
-        console.log(idx)
+        // console.log(idx)
 
         !impar 
         ?(
@@ -40,8 +42,60 @@ const serviceImpar = () => {
 }
 serviceImpar()
 
+
+// onload
+// globalThis.addEventListener('load', () => preloader())
+
+
+
 // function iniciarVideo() {
 //     const video = document.querySelector('.intro-video')
 //     video.play()
 // }
 // iniciarVideo()
+
+
+
+// preloader
+// const preloader = () => {
+//     const preloader =  document.querySelector('.preloader')
+    
+//     setTimeout(() => {
+//         body.classList.toggle('overflow-none')
+//         preloader.classList.remove('grid')
+//         preloader.classList.add('hidde')
+
+//     }, 3000)
+// }
+const deleteLoader = () => {
+    // const body = document.body
+    const loaderCtr = document.querySelector('.preloader')
+    
+    if(loaderCtr.childElementCount > 0){
+        body.classList.toggle('overflow-none')
+
+        // body.classList.remove('ov-hidden')
+        // body.classList.add('overflow-auto')
+
+        loaderCtr.classList.add('animate__fadeOut')
+        
+        
+
+        setTimeout(() => {
+            loaderCtr.classList.remove('w-screen', 'h-screen', 'fixed', 'top-0', 'z-30', 'grid', 'place-items-center', 'bkg-main-black', 'ov-hidden', 'animate__fadeIn')
+            loaderCtr.classList.add('hidde')
+        }, 300)
+
+
+        const arr =  [...loaderCtr.childNodes]
+        
+        arr.forEach(element => {
+            return element.remove()
+        })
+    }
+}
+
+globalThis.addEventListener('load', () => {
+    // const { deleteLoader } = await import('../js/preload/onload.js')
+    return deleteLoader()
+})
