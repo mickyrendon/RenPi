712c89d108a31d5f6e67915b300cb472b108e96e
micky rendon <elderendon@hotmail.es>
2023-09-07 22:01:46 -0300

    [add] animations

--- index.js
+++ index.js
@@ -12,15 +12,7 @@ const body = document.querySelector('body')
 body.addEventListener('click', menuEvent, false)
 serviceImpar()
 
-
-// / JavaScript
-// window.addEventListener('load', function() {
-//   const preloader = document.getElementById('preloader');
-//   preloader.style.display = 'none';
-  
-//   // Mostrar elementos ocultos
-//   const elementos = document.querySelectorAll('.hide');
-//   elementos.forEach(function(elemento) {
-//     elemento.style.display = 'block';
-//   });
-// });
+globalThis.addEventListener("scroll", async () => { 
+    const { handleScrollAnimation } = await import('./src/scrollAnimation.js')
+    return handleScrollAnimation()
+})
